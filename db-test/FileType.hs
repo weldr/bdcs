c9cb8097aaf798d624f4cfedaed7aa5f7d5c9630
Chris Lumens <clumens@redhat.com>
2016-10-28 12:14:10 -0400

    Add a function to convert an Int into a FileType.
    Ideally we'd get this from a library somewhere, but I don't know of any
    that do what we want.  They all call stat on a file and then inspect
    that.  I might at least be able to get the constants from the C header
    file, though.

--- db-test/FileType.hs
+++ db-test/FileType.hs
@@ -2,9 +2,22 @@
 
 module FileType where
 
+import Data.Bits((.&.))
 import Database.Persist.TH
 
 data FileType = RegularFile | Directory | Socket | SymbolicLink | BlockDevice | CharDevice | FIFO
  deriving(Eq, Read, Show)
 
 derivePersistField "FileType"
+
+-- FIXME:  Really need to figure out a library that has this
+getFileType :: Int -> FileType
+getFileType mode = case mode .&. 0o170000 of
+    0o140000 -> Socket
+    0o120000 -> SymbolicLink
+    0o100000 -> RegularFile
+    0o060000 -> BlockDevice
+    0o040000 -> Directory
+    0o020000 -> CharDevice
+    0o010000 -> FIFO
+    _        -> RegularFile
