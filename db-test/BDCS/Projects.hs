5c3d53344a4978018825c5a4741129bb689914f0
David Shea <dshea@redhat.com>
2016-11-11 15:46:26 -0500

    Split the name out of the SRPM name for projects.name

--- db-test/BDCS/Projects.hs
+++ db-test/BDCS/Projects.hs
@@ -8,7 +8,8 @@ module BDCS.Projects(findProject,
 import Control.Monad((>=>))
 import Control.Monad.IO.Class(MonadIO)
 import Data.ByteString.Char8(unpack)
-import Data.Maybe(listToMaybe)
+import Data.List(elemIndices)
+import Data.Maybe(fromJust, listToMaybe)
 import Database.Esqueleto
 
 import BDCS.DB
@@ -27,16 +28,28 @@ insertProject :: MonadIO m => [Tag] -> SqlPersistT m (Key Projects)
 insertProject rpm =
     throwIfNothingOtherwise projectName (DBException "No SourceRPM tag") $
         findProject >=> \case
-            Nothing   -> insert $ mkProject rpm `throwIfNothing` DBException "Couldn't make Projects record"
+            Nothing   -> insert $ mkProject (fromJust projectName) rpm `throwIfNothing` DBException "Couldn't make Projects record"
             Just proj -> return proj
  where
-    -- FIXME:  SourceRPM looks like "pykickstart-2.32-1.fc26.src.rpm", but we really just want "pykickstart"
-    -- here.  I'll get to that.
-    projectName = findStringTag "SourceRPM" rpm
+    -- the closest to a project name we have is the srpm name, e.g., pykickstart-2.32-1.fc26.src.rpm
+    -- This is essentially N-V-R.A.rpm. rpm does not allow hyphens in version of release, and epoch is
+    -- not included in the SRPM name, so we can just take everything before the second-to-last hyphen
+    -- as the name.
+    srpm = findStringTag "SourceRPM" rpm
 
-mkProject :: [Tag] -> Maybe Projects
-mkProject tags = do
-    name        <- projectName
+    srpmToName :: String -> String
+    srpmToName s = 
+        -- Find all the hyphens and take the second to last result
+        let nameHyphenIndex = head $ tail $ reverse $ elemIndices '-' s
+        in fst $ splitAt nameHyphenIndex s
+    
+    -- apply the function to the Maybe from findStringTag
+    projectName = fmap srpmToName srpm
+
+            
+mkProject :: String -> [Tag] -> Maybe Projects
+mkProject projectName tags = do
+    let name     = projectName
     summary     <- findByteStringTag "Summary" tags >>= Just . unpack
     description <- findByteStringTag "Description" tags >>= Just . unpack
     let homepage = findStringTag "URL" tags
@@ -45,7 +58,3 @@ mkProject tags = do
     let upstream_vcs = "UPSTREAM_VCS"
 
     return $ Projects name summary description homepage upstream_vcs
- where
-    -- FIXME:  SourceRPM looks like "pykickstart-2.32-1.fc26.src.rpm", but we really just want "pykickstart"
-    -- here.  I'll get to that.
-    projectName = findStringTag "SourceRPM" tags
