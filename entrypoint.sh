b5899bc1cba1fe870580638efda4afac827d27b2
Chris Lumens <clumens@redhat.com>
2017-04-18 16:11:48 -0400

    Revert "Remove the mddb every time entrypoint.sh runs."
    This reverts commit 0297a62744b6f6599181a6635e7d6b672ee8ba40.  We seem
    to have slowed down our database schema changes.  Plus, importing things
    into the content store takes more time now.  We don't want to re-import
    everything every single time there's a new commit.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ -e /mddb/metadata.db ]; then
-    rm /mddb/metadata.db
-fi
-
 sqlite3 /mddb/metadata.db < /root/schema.sql
 for f in /rpms/*rpm; do
     /usr/local/bin/import /mddb/metadata.db cs.repo file://${f}
