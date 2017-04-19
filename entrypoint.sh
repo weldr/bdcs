da6d1b8267975eb49dc0e67496a9d14f27bdcde8
Chris Lumens <clumens@redhat.com>
2017-04-19 16:08:36 -0400

    Don't unconditionally run sqlite in entrypoint.sh.
    If the mddb already exists, we do not need to make it again.  We are
    only seeing this now because we are using sqlite to create the database
    instead of haskell, plus I have reverted the commit that causes the
    database to be removed every time.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sqlite3 /mddb/metadata.db < /root/schema.sql
+if [ ! -f /mddb/metadata.db ]; then
+    sqlite3 /mddb/metadata.db < /root/schema.sql
+fi
+
 for f in /rpms/*rpm; do
     /usr/local/bin/import /mddb/metadata.db cs.repo file://${f}
 done
