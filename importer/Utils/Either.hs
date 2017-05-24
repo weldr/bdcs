9f80c7aea97ca626abe685c5183a695f5ccfd020
Chris Lumens <clumens@redhat.com>
2017-05-24 15:44:22 -0400

    Add whenLeft and whenRight helpers.
    These do similar things to "when", just for the various Either
    possibilities.  whenRight is not yet used but I can see it happening one
    day.

--- importer/Utils/Either.hs
+++ importer/Utils/Either.hs
@@ -1,4 +1,6 @@
-module Utils.Either(maybeToEither)
+module Utils.Either(maybeToEither,
+                    whenLeft,
+                    whenRight)
  where
 
 import Control.Monad.Except(MonadError, throwError)
@@ -6,3 +8,11 @@ import Control.Monad.Except(MonadError, throwError)
 maybeToEither :: MonadError e m => e -> Maybe a -> m a
 maybeToEither err Nothing = throwError err
 maybeToEither _ (Just v)  = return v
+
+whenLeft :: Monad m => Either e a -> (e -> m ()) -> m ()
+whenLeft (Left a) fn = fn a
+whenLeft _        _  = return ()
+
+whenRight :: Monad m => Either e a -> (a -> m ()) -> m ()
+whenRight (Right a) fn = fn a
+whenRight _         _  = return ()
