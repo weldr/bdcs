67ab04384346bb64c58e20f21158cdfd9dd5e6f6
Chris Lumens <clumens@redhat.com>
2017-06-16 09:56:01 -0400

    Add concatForM.
    This does what you think - concatMapM, but with the arguments in the
    same order as forM.

--- importer/Utils/Monad.hs
+++ importer/Utils/Monad.hs
@@ -1,5 +1,9 @@
-module Utils.Monad(concatMapM)
+module Utils.Monad(concatForM,
+                   concatMapM)
  where
+
+concatForM :: (Monad m, Traversable t) => t a -> (a -> m [b]) -> m [b]
+concatForM lst fn = fmap concat (mapM fn lst)
 
 concatMapM :: (Monad m, Traversable t) => (a -> m [b]) -> t a -> m [b]
 concatMapM fn lst = fmap concat (mapM fn lst)
