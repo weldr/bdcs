56ad199f2fbee3cb9cc69543d708c10672d7ead8
Chris Lumens <clumens@redhat.com>
2017-06-30 09:42:32 -0400

    Remove the haskell-rpm submodule.
    This is no longer needed - the contents of this directory are now an
    official package that can be downloaded from hackage.

--- haskell-rpm/tests/Main.hs
+++
@@ -1,8 +0,0 @@
-module Main(main) where
-
-import           Test.Tasty(defaultMain, testGroup)
-import qualified RPM.Version.Tests
-
-main :: IO ()
-main = defaultMain $ testGroup "Tests"
-    [ RPM.Version.Tests.vercmpTests ]
