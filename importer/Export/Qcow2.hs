ebc03a708ea8cb257805f1e883427201cb53c43c
David Shea <dshea@redhat.com>
2017-07-05 16:25:20 -0400

    Add the ability to export to a qcow2 image.
    This uses virt-make-fs to do the actual image creation. The export needs
    to be run as root since the input to virt-make-fs is the directory
    export, since we still have issues with symlinks in the tar paths.

---
+++ importer/Export/Qcow2.hs
@@ -0,0 +1,47 @@
+-- Copyright (C) 2017 Red Hat, Inc.
+--
+-- This library is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU Lesser General Public
+-- License as published by the Free Software Foundation; either
+-- version 2.1 of the License, or (at your option) any later version.
+--
+-- This library is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+-- Lesser General Public License for more details.
+--
+-- You should have received a copy of the GNU Lesser General Public
+-- License along with this library; if not, see <http://www.gnu.org/licenses/>.
+
+{-# LANGUAGE RankNTypes #-}
+
+module Export.Qcow2(qcow2Sink)
+ where
+
+import Control.Monad.IO.Class(MonadIO, liftIO)
+import Control.Monad.Trans.Resource(MonadResource)
+import Data.Conduit(Consumer, bracketP)
+import System.Directory(removePathForcibly)
+import System.FilePath(takeDirectory)
+import System.IO.Temp(createTempDirectory)
+import System.Process(callProcess)
+
+import qualified BDCS.CS as CS
+import           BDCS.DB(Files)
+import           Export.Directory(directorySink)
+
+qcow2Sink :: (MonadResource m, MonadIO m) => FilePath -> Consumer (Files, CS.Object) m ()
+qcow2Sink outPath =
+    -- Writing and importing a tar file probably will not work, because some rpms contain paths
+    -- with symlinks (e.g., /lib64/libaudit.so.1 is expected to be written to /usr/lib64).
+    -- Instead, export to a temp directory and convert that to qcow
+
+    bracketP (createTempDirectory (takeDirectory outPath) "export")
+        removePathForcibly
+        (\tmpDir -> do
+            -- Run the sink to create a directory export
+            directorySink tmpDir
+
+            -- Run virt-make-fs to generate the qcow2
+            liftIO $ callProcess "virt-make-fs" [tmpDir, outPath, "--format=qcow2", "--label=composer"]
+        )
