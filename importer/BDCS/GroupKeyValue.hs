5be306c603f2c96da900c34903040505aef5417a
David Shea <dshea@redhat.com>
2017-08-15 16:26:42 -0400

    Add functions for fetching group key/val data.

--- importer/BDCS/GroupKeyValue.hs
+++ importer/BDCS/GroupKeyValue.hs
@@ -13,10 +13,13 @@
 -- You should have received a copy of the GNU Lesser General Public
 -- License along with this library; if not, see <http://www.gnu.org/licenses/>.
 
-module BDCS.GroupKeyValue(insertGroupKeyValue)
+module BDCS.GroupKeyValue(getKeyValuesForGroup,
+                          getValueForGroup,
+                          insertGroupKeyValue)
  where
 
 import           Control.Monad.IO.Class(MonadIO)
+import           Data.Maybe(listToMaybe, mapMaybe)
 import qualified Data.Text as T
 import           Database.Esqueleto
 
@@ -29,3 +32,17 @@ insertGroupKeyValue k v e groupId =
     maybeKey (insertKeyValue k (Just v) e >>= \kvId -> insert $ GroupKeyValues groupId kvId)
              (insert . GroupKeyValues groupId)
              (findKeyValue k (Just v) e)
+
+-- Given a group id and a key, return a list of the matching values
+getKeyValuesForGroup :: MonadIO m => Key Groups -> KeyType -> SqlPersistT m [KeyVal]
+getKeyValuesForGroup groupId key = do
+    vals <- select $ from $ \(keyval `InnerJoin` group_keyval) -> do
+            on     $ keyval ^. KeyValId ==. group_keyval ^. GroupKeyValuesKey_val_id
+            where_ $ group_keyval ^. GroupKeyValuesGroup_id ==. val groupId &&.
+                     keyval ^. KeyValKey_value ==. val key
+            return   keyval
+    return $ map entityVal vals
+
+-- Fetch the value for a key/val pair that is expected to occur only once
+getValueForGroup :: MonadIO m => Key Groups -> KeyType -> SqlPersistT m (Maybe T.Text)
+getValueForGroup groupId key = listToMaybe <$> mapMaybe keyValVal_value <$> getKeyValuesForGroup groupId key
