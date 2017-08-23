95d03b664cbe0e23cf073a6a6cd36c07b3c05f50
Chris Lumens <clumens@redhat.com>
2017-08-23 12:25:24 -0400

    Handle exceptions if an inspect subcommand is missing.
    Keep a list of known "built-in" subcommands.  Use that list for the
    usage output.  If a subcommand is provided that isn't in that list, look
    for it in /usr/libexec/weldr.  If something goes wrong running the
    subcommand, catch the exception and print the usage output.

--- importer/tools/inspect/inspect.hs
+++ importer/tools/inspect/inspect.hs
@@ -13,12 +13,25 @@
 -- You should have received a copy of the GNU Lesser General Public
 -- License along with this library; if not, see <http://www.gnu.org/licenses/>.
 
-import Control.Monad(when)
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Control.Conditional(ifM)
+import Control.Exception(SomeException, catch)
+import Control.Monad(forM_, when)
+import System.Directory(doesFileExist)
 import System.Environment(getArgs)
 import System.Exit(exitFailure)
 import System.Process(callProcess)
 
 import BDCS.Version
+
+-- A mapping from a subcommand name to a brief description of what it does.
+knownSubcommands :: [(String, String)]
+knownSubcommands = [
+    ("groups",  "List groups (RPM packages, etc.) in the content store"),
+    ("ls",      "List files in the content store"),
+    ("nevras",  "List NEVRAs of RPM packages in the content store")
+ ]
 
 usage :: IO ()
 usage = do
@@ -27,9 +40,8 @@ usage = do
     putStrLn "- output.db is the path to a metadata database"
     putStrLn "- repo is the path to a content store repo"
     putStrLn "- subcommands:"
-    putStrLn "      groups - List groups (packages, etc.)"
-    putStrLn "      ls     - List files"
-    putStrLn "      nevras - List NEVRAs of RPM packages"
+    forM_ knownSubcommands $ \(cmd, help) ->
+        putStrLn $ "      " ++ cmd ++ " - " ++ help
     exitFailure
 
 {-# ANN main "HLint: ignore Use head" #-}
@@ -41,8 +53,11 @@ main = do
     let subcmd = argv !! 2
     let subcmdArgs = [head argv, argv !! 1] ++ drop 3 argv
 
-    case subcmd of
-        "groups"    -> callProcess "inspect-groups" subcmdArgs
-        "ls"        -> callProcess "inspect-ls" subcmdArgs
-        "nevras"    -> callProcess "inspect-nevras" subcmdArgs
-        _           -> usage
+    case subcmd `lookup` knownSubcommands of
+        Just _  -> tryCallProcess ("inspect-" ++ subcmd) subcmdArgs
+        Nothing -> ifM (doesFileExist ("/usr/libexec/weldr/inspect-" ++ subcmd))
+                       (tryCallProcess ("/usr/libexec/weldr/inspect-" ++ subcmd) subcmdArgs)
+                       usage
+ where
+     tryCallProcess cmd args = catch (callProcess cmd args)
+                                     (\(_ :: SomeException) -> putStrLn ("subcommand " ++ cmd ++ " does not exist\n") >> usage)
