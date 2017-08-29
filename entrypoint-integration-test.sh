f9a1141f9728722733087ebb9150c73975ef18b3
Alexander Todorov <atodorov@redhat.com>
2017-08-29 15:33:36 +0300

    Report coverage for the new depsolve binary

--- entrypoint-integration-test.sh
+++ entrypoint-integration-test.sh
@@ -7,9 +7,12 @@ cd /importer/
 ./tests/test_tmpfiles.sh
 ./tests/test_import.sh
 ./tests/test_export.sh
+./tests/test_depsolve.sh
 
 # collect coverage data from unit tests and binaries
 mkdir ./dist/hpc/vanilla/tix/bdcs-tmpfiles/ ./dist/hpc/vanilla/tix/import/ ./dist/hpc/vanilla/tix/export/
+mkdir ./dist/hpc/vanilla/tix/depsolve/
 mv bdcs-tmpfiles.tix ./dist/hpc/vanilla/tix/bdcs-tmpfiles/
 mv import.tix ./dist/hpc/vanilla/tix/import/
 mv export.tix ./dist/hpc/vanilla/tix/export/
+mv depsolve.tix ./dist/hpc/vanilla/tix/depsolve/
