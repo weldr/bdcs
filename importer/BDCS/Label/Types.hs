57071d2afa97dfbed30db7379246d7000a5f488e
Chris Lumens <clumens@redhat.com>
2017-08-29 13:18:11 -0400

    Add functions for converting a KeyType to Text.
    KeyType already implements Show, but that's not really so good for
    output since it involves extra double quotes.  Labels are fine for now
    since they don't store any extra information.

--- importer/BDCS/Label/Types.hs
+++ importer/BDCS/Label/Types.hs
@@ -13,6 +13,7 @@
 -- You should have received a copy of the GNU Lesser General Public
 -- License along with this library; if not, see <http://www.gnu.org/licenses/>.
 
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module BDCS.Label.Types(Label(..))
