553ddbd739f121ddd26f3f20f0402698dec63005
Chris Lumens <clumens@redhat.com>
2017-09-21 15:39:57 -0400

    Add a function to get a single Projects row, given its ID.
    This should be composable with the findProject function, by doing
    something like:
    
    p <- findProject "anaconda" >>= getProject
    
    This could be a useful pattern to do throughout the BDCS code.  I
    intended to write this kind of function a while ago but never got around
    to it.

--- importer/BDCS/Projects.hs
+++ importer/BDCS/Projects.hs
@@ -16,10 +16,12 @@
 {-# LANGUAGE RecordWildCards #-}
 
 module BDCS.Projects(findProject,
+                     getProject,
                      insertProject)
  where
 
 import           Control.Monad.IO.Class(MonadIO)
+import           Data.Maybe(listToMaybe)
 import qualified Data.Text as T
 import           Database.Esqueleto
 
@@ -31,6 +33,14 @@ findProject name = firstResult $
     where_ $ proj ^. ProjectsName ==. val name
     limit 1
     return $ proj ^. ProjectsId
+
+getProject :: MonadIO m => Key Projects -> SqlPersistT m (Maybe Projects)
+getProject key = do
+    results <- select $ from $ \proj -> do
+               where_ $ proj ^. ProjectsId ==. val key
+               limit 1
+               return proj
+    return $ listToMaybe (map entityVal results)
 
 insertProject :: MonadIO m => Projects -> SqlPersistT m (Key Projects)
 insertProject project@Projects{..} =
