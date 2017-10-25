70f7b250a5117e101c3759c1bcdf0f9d608866e8
David Shea <dshea@redhat.com>
2017-10-25 15:47:18 -0400

    Remove commitContentToFile
    This function was only useful when npm was unable to stream data into
    the content-store from the original archive. Using the file metadata
    from the original archive is better than trying to get it off the
    filesystem, and removing the function cleans up BDCS.CS a good bit.

--- importer/BDCS/CS.hs
+++ importer/BDCS/CS.hs
@@ -6,14 +6,12 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module BDCS.CS(Object(..),
-               commitContentToFile,
                filesToObjectsC,
                objectToTarEntry)
  where
 
 import qualified Codec.Archive.Tar as Tar
 import qualified Codec.Archive.Tar.Entry as Tar
-import           Control.Conditional(ifM)
 import           Control.Monad.Except(ExceptT(..), MonadError, runExceptT, throwError)
 import           Control.Monad.IO.Class(MonadIO, liftIO)
 import qualified Data.ByteString as BS
@@ -21,8 +19,6 @@ import           Data.ByteString.Lazy(fromStrict)
 import           Data.Conduit(Conduit, awaitForever, yield)
 import           Data.ContentStore(ContentStore, fetchByteString, runCsMonad)
 import qualified Data.Text as T
-import           System.FilePath((</>), isAbsolute, makeRelative, normalise)
-import           System.Posix.Files(getFileStatus, getSymbolicLinkStatus, isSymbolicLink, modificationTimeHiRes, readSymbolicLink)
 import           System.Posix.Types(CMode(..))
 
 import BDCS.DB
@@ -83,26 +79,3 @@ objectToTarEntry = awaitForever $ \(f@Files{..}, obj) -> do
                                                        Tar.ownerName = T.unpack filesFile_user,
                                                        Tar.groupName = T.unpack filesFile_group },
                 Tar.entryTime = fromIntegral filesMtime }
-
--- | Create a "Files" record for the given path
-commitContentToFile :: FilePath         -- ^ A path prefix to use as the root of the file import
-                    -> (T.Text, T.Text) -- ^ The path to convert, relative to the prefix, and the content store checksum
-                    -> IO Files         -- ^ The resulting record
-commitContentToFile prefix (path, checksum) = do
-    -- combine the prefix and the path. If the path already starts with a /, remove it
-    let normalPrefix = normalise prefix
-    let normalPath   = normalise (T.unpack path)
-    let relativePath = if isAbsolute normalPath then makeRelative "/" normalPath else normalPath
-    let fullPath = normalPrefix </> relativePath
-
-    -- use getSymbolicLinkStatus (lstat) since there may be broken symlinks
-    stat <- getSymbolicLinkStatus fullPath
-    let mtime = floor $ modificationTimeHiRes stat
-
-    target <- ifM (isSymbolicLink <$> getFileStatus fullPath)
-                  (Just <$> readSymbolicLink fullPath)
-                  (return Nothing)
-
-    -- TODO user/group/mode/size
-
-    return $ Files (T.pack ("/" </> relativePath)) "root" "root" mtime (Just checksum) 0 0 (fmap T.pack target)
