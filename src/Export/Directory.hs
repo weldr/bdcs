aa82b2fe9a087092bb9742eda7e0cf44dada225d
Chris Lumens <clumens@redhat.com>
2017-11-13 16:00:48 -0500

    Take care of an hlint warning that was killing docker.
    This appears to be due to a new version of hlint that docker picks up
    but that I do not have installed locally.

--- src/Export/Directory.hs
+++ src/Export/Directory.hs
@@ -14,6 +14,7 @@
 -- License along with this library; if not, see <http://www.gnu.org/licenses/>.
 
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE RecordWildCards #-}
@@ -38,7 +39,7 @@ import           BDCS.DB
 import           Utils.Filesystem(doesPathExist)
 
 directorySink :: (MonadError String m, MonadIO m) => FilePath -> Consumer (Files, CS.Object) m ()
-directorySink outPath = awaitForever $ \obj -> case obj of
+directorySink outPath = awaitForever $ \case
     (f, CS.SpecialObject) -> checkoutSpecial f
     (f, CS.FileObject bs) -> checkoutFile f bs
  where
