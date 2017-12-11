84dce3ca09b352ea81a8b0800efb74ea6bb50c8a
Chris Lumens <clumens@redhat.com>
2017-12-11 14:16:33 -0500

    Reduce code duplication between bdcs and inspect commands.
    They're doing much the same thing in handling subcommands, so this code
    can be broken out into its own module and the duplicate copies removed.

--- src/tools/inspect/inspect.hs
+++ src/tools/inspect/inspect.hs
@@ -15,24 +15,15 @@
 
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 
-import Control.Conditional(condM, ifM, otherwiseM)
-import Control.Exception(SomeException, catch)
 import Control.Monad(forM_, when)
-import Control.Monad.Loops(firstM)
-import Data.Text(pack, splitOn, unpack)
-import System.Directory(doesFileExist)
-import System.Environment(getArgs, lookupEnv)
+import System.Environment(getArgs)
 import System.Exit(exitFailure)
-import System.FilePath((</>))
-import System.Process(callProcess)
 
 import BDCS.Version
 
 import Utils.GetOpt(commandLineArgs)
-
-import Paths_bdcs(getLibexecDir)
+import Utils.Subcommands(runSubcommand)
 
 -- A mapping from a subcommand name to a brief description of what it does.
 knownSubcommands :: [(String, String)]
@@ -44,7 +35,7 @@ knownSubcommands = [
 
 usage :: IO ()
 usage = do
-    printVersion "bdcs-inspect"
+    printVersion "inspect"
     putStrLn "Usage: inspect output.db repo subcommand [args ...]"
     putStrLn "- output.db is the path to a metadata database"
     putStrLn "- repo is the path to a content store repo"
@@ -53,65 +44,14 @@ usage = do
         putStrLn $ "      " ++ cmd ++ " - " ++ help
     exitFailure
 
-getBasePath :: IO FilePath
-getBasePath = do
-    dir <- getLibexecDir
-    return $ dir </> "inspect-"
-
-findInPath :: FilePath -> IO (Maybe FilePath)
-findInPath sought = lookupEnv "PATH" >>= \case
-    Nothing -> return Nothing
-    Just p  -> do let searchPath = splitOn ":" (pack p)
-                  fmap (\d -> unpack d </> sought) <$>
-                       firstM (\d -> doesFileExist (unpack d </> sought)) searchPath
-
-existsInPath :: FilePath -> IO Bool
-existsInPath sought = fmap (/= Nothing) (findInPath sought)
-
-runCommand :: FilePath -> FilePath -> [String] -> IO ()
-runCommand db repo args = do
-    let subcmd = head args
-    let subcmdArgs = [db, repo] ++ tail args
-
-    basePath <- getBasePath
-
-    let cmd1 = basePath ++ subcmd
-    let cmd2 = "inspect-" ++ subcmd
-
-    case subcmd `lookup` knownSubcommands of
-        -- This is a subcommand we have built-in knowledge of.  For ease of development, it
-        -- could be located in a couple different places:  Installed in /usr/libexec/weldr,
-        -- or in the $PATH.  The latter allows us to run "PATH=$PATH:dist/build/... cabal run"
-        -- without having to run "cabal install" every time we want to test something.
-        --
-        -- While tryCallProcess will search the $PATH itself, we need to know if the file
-        -- exists somewhere in the $PATH first.  This allows us to separate file not found
-        -- errors from the command failing.
-        Just _  -> condM [(doesFileExist cmd1, tryCallProcess cmd1 subcmdArgs),
-                          (existsInPath cmd1,  tryCallProcess cmd1 subcmdArgs),
-                          (doesFileExist cmd2, tryCallProcess cmd2 subcmdArgs),
-                          (existsInPath cmd2,  tryCallProcess cmd2 subcmdArgs),
-                          (otherwiseM,         putStrLn ("subcommand " ++ subcmd ++ " does not exist\n") >> usage)]
-
-        -- This is a subcommand we know nothing about.  Check to see if it exists in
-        -- /usr/libexec/weldr, since it could have been installed by a third party.  If so,
-        -- run that.  If not, display an error message and quit.
-        Nothing -> ifM (doesFileExist cmd1)
-                       (tryCallProcess cmd1 subcmdArgs)
-                       (putStrLn ("subcommand " ++ subcmd ++ " does not exist\n") >> usage)
- where
-     tryCallProcess cmd opts = catch (callProcess cmd opts)
-                                     -- We handled the case where an unknown subcommand was
-                                     -- given on the command line.  For now, the only other
-                                     -- errors possible are when the subcommand ran, but
-                                     -- failed for some reason.  Those are handled inside
-                                     -- the subcommand.  Just quit.
-                                     (\(_ :: SomeException) -> exitFailure)
-
 main :: IO ()
 main = commandLineArgs <$> getArgs >>= \case
     Just (db, repo, args) -> do
         when (null args) usage
 
-        runCommand db repo args
+        let subcmd = head args
+            subcmdArgs = [db, repo] ++ tail args
+
+        runSubcommand "inspect-" subcmd subcmdArgs knownSubcommands usage
+
     _                     -> usage
