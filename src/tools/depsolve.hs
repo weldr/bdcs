593c19063a5e5ce7c1d2f281578236c7384eb877
Chris Lumens <clumens@redhat.com>
2017-12-11 11:09:48 -0500

    Add a top-level bdcs command.
    Then, all the other commands become subcommands, working just like the
    inspect command already does.  Part of the reason for this is so we have
    one entry point from the command line to all our tools.  Another reason
    is to rename our very generic commands to something less generic and put
    them into a special location so they can be installed onto a system
    without creating conflicts.
    
    There's some code duplication between the bdcs and inspect programs
    here.  I hope to find ways to reduce that in future commits.

--- src/tools/depsolve.hs
+++ src/tools/depsolve.hs
@@ -13,9 +13,9 @@
 -- You should have received a copy of the GNU Lesser General Public
 -- License along with this library; if not, see <http://www.gnu.org/licenses/>.
 
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-import           Control.Monad(when)
 import           Control.Monad.Except(runExceptT)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
@@ -28,21 +28,13 @@ import BDCS.Depsolve(formulaToCNF, solveCNF)
 import BDCS.Groups(groupIdToNevra)
 import BDCS.Utils.Monad(mapMaybeM)
 
-usage :: IO ()
-usage = do
-    putStrLn "Usage: depsolve metadata.db NEVRA [NEVRA ...]"
-    exitFailure
+import Utils.GetOpt(commandLineArgs)
 
-main :: IO ()
-main = do
-    argv <- getArgs
-
-    when (length argv < 2) usage
-
-    let db = T.pack $ head argv
-    let things = map T.pack $ drop 1 argv
-    result <- runExceptT $ checkAndRunSqlite db $ do
-        formula <- depclose ["x86_64"] things
+runCommand :: FilePath -> [String] -> IO ()
+runCommand db things = do
+    let things' = map T.pack things
+    result <- runExceptT $ checkAndRunSqlite (T.pack db) $ do
+        formula <- depclose ["x86_64"] things'
         solution <- solveCNF (formulaToCNF formula)
 
         -- solveCNF returns a list of (groupId, bool) assignments. Discard the False ones,
@@ -53,3 +45,13 @@ main = do
         Left e            -> print ("error: " ++ e) >> exitFailure
         -- Print the NEVRAs one per line
         Right assignments -> mapM_ TIO.putStrLn assignments
+
+usage :: IO ()
+usage = do
+    putStrLn "Usage: depsolve metadata.db NEVRA [NEVRA ...]"
+    exitFailure
+
+main :: IO ()
+main = commandLineArgs <$> getArgs >>= \case
+    Just (db, _, args) -> runCommand db args
+    _                  -> usage
