ac930874fe90537009a7b9805191970992035633
Alexander Todorov <atodorov@redhat.com>
2017-12-13 13:51:23 +0200

    [test] Import RPM from local disk

--- tests/test_import.sh
+++ tests/test_import.sh
@@ -14,6 +14,14 @@ METADATA_DB="./import_metadata.db"
 sqlite3 $METADATA_DB < ../schema.sql
 CENTOS_REPO="centos.repo"
 
+### test that we can import a single RPM from local disk
+wget http://mirror.centos.org/centos/7/os/x86_64/Packages/setup-2.8.71-7.el7.noarch.rpm && \
+    $IMPORT $METADATA_DB $CENTOS_REPO file://`pwd`/setup-2.8.71-7.el7.noarch.rpm
+if [ $? -ne 0 ]; then
+    echo "ERROR: importing from local disk failed"
+    exit 1
+fi
+
 ### test that we can import a single RPM from HTTPS URL
 $BDCS import $METADATA_DB $CENTOS_REPO https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/a/abc-1.01-9.hg20160905.el7.x86_64.rpm
 if [ $? -ne 0 ]; then
