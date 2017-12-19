f137a8ae3b55dafbd17c2ffee573d503b901ebba
Chris Lumens <clumens@redhat.com>
2017-12-19 11:03:26 -0500

    Add documentation to a couple more top-level files.

--- src/BDCS/KeyType.hs
+++ src/BDCS/KeyType.hs
@@ -1,19 +1,15 @@
--- Copyright (C) 2017 Red Hat, Inc.
---
--- This library is free software; you can redistribute it and/or
--- modify it under the terms of the GNU Lesser General Public
--- License as published by the Free Software Foundation; either
--- version 2.1 of the License, or (at your option) any later version.
---
--- This library is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
--- Lesser General Public License for more details.
---
--- You should have received a copy of the GNU Lesser General Public
--- License along with this library; if not, see <http://www.gnu.org/licenses/>.
-
 {-# LANGUAGE TemplateHaskell #-}
+
+-- |
+-- Module: BDCS.ReqType
+-- Copyright: (c) 2017 Red Hat, Inc.
+-- License: LGPL
+--
+-- Maintainer: https://github.com/weldr
+-- Stability: alpha
+-- Portability: portable
+--
+-- Data types for working with the key in a 'BDCS.DB.KeyVal'.
 
 module BDCS.KeyType(KeyType(..),
                     asText)
@@ -25,8 +21,11 @@ import qualified Data.Text as T
 
 import qualified BDCS.Label.Types as Label
 
-data KeyType = LabelKey Label.Label
-             | TextKey T.Text
+-- | The 'BDCS.DB.KeyVal' record features a namespaced key, allowing multiple types of keys
+-- to exist and be managed in the same database.  This type allows differentiation of the
+-- multiple kinds of keys.
+data KeyType = LabelKey Label.Label     -- ^ A key allowing only pre-defined labels to be appplied.
+             | TextKey T.Text           -- ^ Ordinary text key
  deriving(Eq, Read, Show)
 
 derivePersistField "KeyType"
@@ -35,6 +34,7 @@ instance ToJSON KeyType where
     toJSON (TextKey t)  = toJSON t
     toJSON (LabelKey l) = toJSON l
 
+-- | Convert a 'KeyType' into 'Data.Text.Text'
 asText :: KeyType -> T.Text
 asText (LabelKey l) = T.pack $ show l
 asText (TextKey t)  = t
