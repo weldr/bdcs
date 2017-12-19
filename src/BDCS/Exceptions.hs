a5d220e14ecb740bf902284ac3bb79214fe983ef
Chris Lumens <clumens@redhat.com>
2017-12-19 11:03:26 -0500

    Add documentation to a couple more top-level files.

--- src/BDCS/Exceptions.hs
+++ src/BDCS/Exceptions.hs
@@ -1,17 +1,13 @@
--- Copyright (C) 2016-2017 Red Hat, Inc.
+-- |
+-- Module: BDCS.Builds
+-- Copyright: (c) 2016-2017 Red Hat, Inc.
+-- License: LGPL
 --
--- This library is free software; you can redistribute it and/or
--- modify it under the terms of the GNU Lesser General Public
--- License as published by the Free Software Foundation; either
--- version 2.1 of the License, or (at your option) any later version.
+-- Maintainer: https://github.com/weldr
+-- Stability: alpha
+-- Portability: portable
 --
--- This library is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
--- Lesser General Public License for more details.
---
--- You should have received a copy of the GNU Lesser General Public
--- License along with this library; if not, see <http://www.gnu.org/licenses/>.
+-- Utilities for working with database-related exceptions.
 
 module BDCS.Exceptions(DBException(..),
                        isDBExceptionException,
@@ -23,13 +19,15 @@ module BDCS.Exceptions(DBException(..),
 import Control.Exception(Exception, throw)
 import Data.Data(Typeable)
 
--- A general purpose exception type for dealing with things that go wrong when working
--- with the database.  This could be broken out into a lot more type constructors to
--- make for an actually useful exception system.  In general, I dislike Haskell exceptions
--- but runSqlite will roll back the entire transaction if an exception is raised.  That's
--- a good reason to use them.
-data DBException = DBException String
-                 | MissingRPMTag String
+-- | A general purpose exception type for dealing with things that go wrong when working
+-- with the database.  This type could grow into a more complex system in the future, if
+-- needed.  This type is most helpful because runSqlite will roll back the entire
+-- transaction if an exception is raised.
+data DBException = DBException String           -- ^ A general purpose exception type,
+                                                -- including an error message.
+                 | MissingRPMTag String         -- ^ A required tag was missing from the
+                                                -- RPM being processed.  The argument should
+                                                -- be the name of the missing tag.
  deriving(Eq, Typeable)
 
 instance Exception DBException
@@ -38,18 +36,24 @@ instance Show DBException where
     show (DBException s)   = show s
     show (MissingRPMTag s) = "Missing required tag in RPM: " ++ s
 
+-- | If a 'Maybe' value is Nothing, throw the given exception.  Otherwise, return the
+-- value inside.
 throwIfNothing :: Exception e => Maybe a -> e -> a
 throwIfNothing (Just v) _   = v
 throwIfNothing _        exn = throw exn
 
+-- | If a 'Maybe' value is Nothing, throw the given exception.  Otherwise, run the
+-- provided function on the value inside and return the result.
 throwIfNothingOtherwise :: Exception e => Maybe a -> e -> (a -> b) -> b
 throwIfNothingOtherwise (Just v) _   fn = fn v
 throwIfNothingOtherwise _        exn _  = throw exn
 
+-- | Is a given 'DBException' type the general 'DBException'?
 isDBExceptionException :: DBException -> Bool
 isDBExceptionException (DBException _) = True
 isDBExceptionException _               = False
 
+-- | Is a given 'DBException' type a 'MissingRPMTag'?
 isMissingRPMTagException :: DBException -> Bool
 isMissingRPMTagException (MissingRPMTag _) = True
 isMissingRPMTagException _                 = False
