d02ce5bfb967110fbd4886df31b0ea49d59d36ee
Alexander Todorov <atodorov@redhat.com>
2017-12-19 14:22:38 +0200

    Build our own metadata.db for depsolve testing
    instead of downloading the huge CentOS metadata.db from AWS S3,
    which btw is nearly always broken, the test resolves the
    dependencies and downloads pacakges with dnf, then imports them
    into a smaller DB and uses that for testing!

--- tests/test_depsolve.sh
+++ tests/test_depsolve.sh
@@ -20,18 +20,34 @@ fi
 ### Prepare for testing depsolve against recipes
 
 METADATA_DB="metadata.db"
-[ -f "$METADATA_DB" ] || curl https://s3.amazonaws.com/weldr/metadata.db > "$METADATA_DB"
+CS_REPO="/tmp/depsolve.repo"
+sqlite3 $METADATA_DB < ../schema.sql
+
+DNF_ROOT=`mktemp -d /tmp/dnf.root.XXXXXX`
+DNF_DOWNLOAD=`mktemp -d /tmp/dnf.download.XXXXXX`
+
+# first depsolve via dnf and download all the RPMs
+sudo dnf install -y --nogpgcheck --releasever=26 --downloadonly --downloaddir=$DNF_DOWNLOAD --installroot=$DNF_ROOT httpd
+
+# then import all RPMs
+for F in $DNF_DOWNLOAD/*.rpm; do
+    $BDCS import $METADATA_DB $CS_REPO file://$F
+done
+
+# figure out the exact NEVRA for the httpd package
+HTTPD_NEVRA=`find $DNF_DOWNLOAD -type f -name "httpd-2*.rpm" | cut -f4 -d/ | sed 's/\.rpm//'`
+
 
 # when called with correct parameters
 # then output contains input nevra
 # and the return code is 0
-OUTPUT=`$BDCS depsolve $METADATA_DB httpd-2.4.6-67.el7.centos.x86_64`
+OUTPUT=`$BDCS depsolve $METADATA_DB $HTTPD_NEVRA`
 if [[ $? != 0 ]]; then
     echo "FAIL: Return code is not zero"
     exit 1
 fi
 
-echo "$OUTPUT" | grep httpd-2.4.6-67.el7.centos.x86_64
+echo "$OUTPUT" | grep $HTTPD_NEVRA
 if [ $? != 0 ]; then
     echo "FAIL: httpd not included in depsolved list"
     exit 1
