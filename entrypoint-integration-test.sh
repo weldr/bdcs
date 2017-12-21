498633a5c4d7d74c38d4edac3904be0e121d106c
Alexander Todorov <atodorov@redhat.com>
2017-12-21 15:44:16 +0200

    Use the correct names for coverage files

--- entrypoint-integration-test.sh
+++ entrypoint-integration-test.sh
@@ -10,7 +10,9 @@ cd /bdcs/
 ./tests/test_depsolve.sh
 
 # collect coverage data from unit tests and binaries
-mkdir ./dist/hpc/vanilla/tix/bdcs-tmpfiles/ ./dist/hpc/vanilla/tix/import/ ./dist/hpc/vanilla/tix/export/
+mkdir ./dist/hpc/vanilla/tix/bdcs-tmpfiles/
+mkdir ./dist/hpc/vanilla/tix/bdcs-import/
+mkdir ./dist/hpc/vanilla/tix/bdcs-export/
 mkdir ./dist/hpc/vanilla/tix/bdcs-depsolve/
 mv bdcs-tmpfiles.tix ./dist/hpc/vanilla/tix/bdcs-tmpfiles/
 mv bdcs-import.tix ./dist/hpc/vanilla/tix/bdcs-import/
