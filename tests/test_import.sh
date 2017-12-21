2493feadb5fb6452608b163e03b54ddc34b99acc
Alexander Todorov <atodorov@redhat.com>
2017-12-21 16:38:16 +0200

    Update URL for package import

--- tests/test_import.sh
+++ tests/test_import.sh
@@ -23,7 +23,7 @@ if [ $? -ne 0 ]; then
 fi
 
 ### test that we can import a single RPM from HTTPS URL
-$BDCS import $METADATA_DB $CENTOS_REPO https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/a/abc-1.01-9.hg20160905.el7.x86_64.rpm
+$BDCS import $METADATA_DB $CENTOS_REPO https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/a/abc-1.01-9.hg20160905.el7.x86_64.rpm
 if [ $? -ne 0 ]; then
     echo "ERROR: importing from HTTPS URL failed"
     exit 1
