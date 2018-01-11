fee7da049f38eb34f59c78463e07d05990861034
Alexander Todorov <atodorov@redhat.com>
2018-01-11 13:06:47 +0200

    New test: when importing from 404 URL exit code should be != 0

--- tests/test_import.sh
+++ tests/test_import.sh
@@ -18,6 +18,11 @@ rlJournalStart
         rlAssertEquals "Usage header as expected" "$output" "Usage: import output.db repo thing [thing ...]"
     rlPhaseEnd
 
+    rlPhaseStartTest "When importing from URL which gives a 404 exit code should be non-zero"
+        $BDCS import $METADATA_DB $CENTOS_REPO http://mirror.centos.org/centos/7/os/x86_64/Packages/NON-EXISTING-RPM.rpm
+        rlAssertNotEquals "On error exit code should not be zero" $? 0
+    rlPhaseEnd
+
     rlPhaseStartTest "Can import a single RPM from local disk"
         rlRun -t -c "wget http://mirror.centos.org/centos/7/os/x86_64/Packages/setup-2.8.71-7.el7.noarch.rpm"
         rlRun -t -c "$BDCS import $METADATA_DB $CENTOS_REPO file://`pwd`/setup-2.8.71-7.el7.noarch.rpm"
