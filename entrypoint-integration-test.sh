082851708be70b79cddf176c24530a302074d8dc
Alexander Todorov <atodorov@redhat.com>
2018-02-06 15:49:51 +0200

    Share ~/.cabal-sandbox dir between host and docker container
    this should speed up the builds and prevent rebuilding of all
    Haskell dependencies in Travis.

--- entrypoint-integration-test.sh
+++ entrypoint-integration-test.sh
@@ -5,16 +5,16 @@ set -ex
 cd /bdcs/
 
 ./tests/test_tmpfiles.sh
-grep RESULT_STRING=PASS /var/tmp/beakerlib-*/TestResults || exit 1
+grep RESULT_STRING /var/tmp/beakerlib-*/TestResults | grep -v PASS && exit 1
 
 ./tests/test_import.sh
-grep RESULT_STRING=PASS /var/tmp/beakerlib-*/TestResults || exit 1
+grep RESULT_STRING /var/tmp/beakerlib-*/TestResults | grep -v PASS && exit 1
 
 ./tests/test_export.sh
-grep RESULT_STRING=PASS /var/tmp/beakerlib-*/TestResults || exit 1
+grep RESULT_STRING /var/tmp/beakerlib-*/TestResults | grep -v PASS && exit 1
 
 ./tests/test_depsolve.sh
-grep RESULT_STRING=PASS /var/tmp/beakerlib-*/TestResults || exit 1
+grep RESULT_STRING /var/tmp/beakerlib-*/TestResults | grep -v PASS && exit 1
 
 # collect coverage data from unit tests and binaries
 mkdir ./dist/hpc/vanilla/tix/bdcs-tmpfiles/
