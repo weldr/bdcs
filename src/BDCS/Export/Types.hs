65e9c6b52acede7d47bfd83f3c3d1366ed6cd328
Chris Lumens <clumens@redhat.com>
2018-04-10 14:23:26 -0400

    Add a new BDCS.Export.Types module.
    For now, this just provides an ExportType data type to keep track of
    what kind of output is being requested, without having to deal with
    string comparisons.

---
+++ src/BDCS/Export/Types.hs
@@ -0,0 +1,43 @@
+-- |
+-- Module: BDCS.Export.Types
+-- Copyright: (c) 2018 Red Hat, Inc.
+-- License: LGPL
+--
+-- Maintainer: https://github.com/weldr
+-- Stability: alpha
+-- Portability: portable
+--
+-- Types related to exporting.
+
+{-# LANGUAGE OverloadedStrings #-}
+
+module BDCS.Export.Types(ExportType(..),
+                         exportTypeText,
+                         exportTypeFromText,
+                         supportedExportTypes)
+ where
+
+import qualified Data.Text as T
+
+data ExportType = ExportDirectory
+                | ExportOstree
+                | ExportQcow2
+                | ExportTar
+ deriving(Eq, Show)
+
+exportTypeText :: ExportType -> T.Text
+exportTypeText ExportDirectory = "directory"
+exportTypeText ExportOstree    = "ostree"
+exportTypeText ExportQcow2     = "qcow2"
+exportTypeText ExportTar       = "tar"
+
+exportTypeFromText :: T.Text -> Maybe ExportType
+exportTypeFromText t = case T.toLower (T.strip t) of
+    "directory" -> Just ExportDirectory
+    "ostree"    -> Just ExportOstree
+    "qcow2"     -> Just ExportQcow2
+    "tar"       -> Just ExportTar
+    _           -> Nothing
+
+supportedExportTypes :: [ExportType]
+supportedExportTypes = [ExportDirectory, ExportOstree, ExportQcow2, ExportTar]
