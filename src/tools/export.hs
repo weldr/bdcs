2fa4dea6a0d37c53e43f74bde9c8604573a0d876
Chris Lumens <clumens@redhat.com>
2018-04-12 10:48:10 -0400

    Add an option to the export command to specify the export type.
    This breaks apart the existing link between the name of the export
    artifact and the type of the artifact.  You can now give any name you
    want to any type you want.
    
    This changes the command line interface for the export command.  You
    must now give "-t <some type>".  Because the number of things you need
    to provide on the command line continues to grow, I've also made it so
    the destination is given via "-d <dest>".  Finally, error handling on
    command line options should be better.

--- src/tools/export.hs
+++ src/tools/export.hs
@@ -1,4 +1,4 @@
--- Copyright (C) 2017 Red Hat, Inc.
+-- Copyright (C) 2017-2018 Red Hat, Inc.
 --
 -- This library is free software; you can redistribute it and/or
 -- modify it under the terms of the GNU Lesser General Public
@@ -14,22 +14,46 @@
 -- License along with this library; if not, see <http://www.gnu.org/licenses/>.
 
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
-import           Control.Conditional(cond, ifM)
+import           Control.Conditional(ifM)
 import           Control.Monad.Except(runExceptT)
-import           Data.List(isSuffixOf)
+import           Data.Maybe(fromJust, isNothing)
 import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import           System.Console.GetOpt
 import           System.Directory(doesFileExist, removePathForcibly)
 import           System.Environment(getArgs)
 import           System.Exit(exitFailure, exitSuccess)
 
 import BDCS.DB(checkAndRunSqlite)
 import BDCS.Export(export)
-import BDCS.Export.Types(ExportType(..))
+import BDCS.Export.Types(exportTypeText, exportTypeFromText, supportedExportTypes)
 import BDCS.Utils.Monad(concatMapM)
 import BDCS.Version
 
-import Utils.GetOpt(commandLineArgs)
+import Utils.GetOpt(OptClass, commandLineArgs, compilerOpts)
+
+data ExportOpts = ExportOpts { optDest :: FilePath,
+                               optExportType :: T.Text }
+
+instance OptClass ExportOpts
+
+defaultExportOpts :: ExportOpts
+defaultExportOpts = ExportOpts { optDest = "",
+                                 optExportType = "tar" }
+
+options :: [OptDescr (ExportOpts -> ExportOpts)]
+options = [
+    Option ['d'] ["dest"]
+           (ReqArg (\d opts -> opts { optDest = d }) "DEST")
+           "destination",
+    Option ['t'] ["type"]
+           (ReqArg (\t opts -> opts { optExportType = T.pack t }) "TYPE")
+           "export type"
+ ]
 
 -- | Check a list of strings to see if any of them are files
 -- If it is, read it and insert its contents in its place
@@ -41,32 +65,41 @@ expandFileThings = concatMapM isThingFile
                             (lines <$> readFile thing)
                             (return [thing])
 
+validExportTypes :: T.Text
+validExportTypes = T.intercalate ", " (map exportTypeText supportedExportTypes)
+
 usage :: IO ()
 usage = do
     printVersion "export"
-    putStrLn "Usage: export metadata.db repo dest thing [thing ...]"
-    putStrLn "dest can be:"
-    putStrLn "\t* A directory (which may or may not already exist)"
-    putStrLn "\t* The name of a .tar file to be created"
-    putStrLn "\t* The name of a .qcow2 image to be created"
-    putStrLn "\t* A directory ending in .repo, which will create a new ostree repo"
-    putStrLn "thing can be:"
-    putStrLn "\t* The NEVRA of an RPM, e.g. filesystem-3.2-21.el7.x86_64"
-    putStrLn "\t* A path to a file containing NEVRA of RPMs, 1 per line."
+    putStrLn   "Usage: export metadata.db repo -t [export type] -d [dest] thing [thing ...]"
+    putStrLn   "metadata.db - The path to an existing metadata repo"
+    putStrLn   "repo        - The path to an existing content store"
+    putStrLn $ "export type - One of the supported export types: " ++ T.unpack validExportTypes
+    putStrLn   "dest        - The name of the export artifact to be created"
+    putStrLn   "thing       -"
+    putStrLn   "\t* The NEVRA of an RPM, e.g. filesystem-3.2-21.el7.x86_64"
+    putStrLn   "\t* A path to a file containing NEVRA of RPMs, 1 per line."
     -- TODO group id?
     exitFailure
 
 main :: IO ()
 main = commandLineArgs <$> getArgs >>= \case
-    Just (db, repo, out_path:things) -> do things' <- map T.pack <$> expandFileThings things
+    Nothing               -> usage
+    Just (db, repo, args) -> do
+        (ExportOpts{..}, things) <- compilerOpts options defaultExportOpts args "export"
+        things'                  <- map T.pack <$> expandFileThings things
 
-                                           let ty = cond [(".tar" `isSuffixOf` out_path,   ExportTar),
-                                                          (".qcow2" `isSuffixOf` out_path, ExportQcow2),
-                                                          (".repo" `isSuffixOf` out_path,  ExportOstree),
-                                                          (otherwise,                      ExportDirectory)]
-
-                                           result  <- runExceptT $ checkAndRunSqlite (T.pack db) $ export repo out_path ty things'
-                                           case result of
-                                               Left err -> removePathForcibly out_path >> print err >> exitFailure
-                                               Right _  -> exitSuccess
-    _                                -> usage
+        if | isNothing (exportTypeFromText optExportType) -> do
+                 TIO.putStrLn $ "Invalid export type.  Valid types are: " `T.append` validExportTypes
+                 exitFailure
+           | null optDest -> do
+                 putStrLn "No export destination given."
+                 exitFailure
+           | null things' -> do
+                 putStrLn "Nothing to export."
+                 exitFailure
+           | otherwise -> do
+                 let ty = fromJust $ exportTypeFromText optExportType
+                 runExceptT (checkAndRunSqlite (T.pack db) $ export repo optDest ty things') >>= \case
+                     Left err -> removePathForcibly optDest >> print err >> exitFailure
+                     Right _  -> exitSuccess
